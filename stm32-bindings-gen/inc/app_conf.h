c3a07d030753ec93a2153a05a6f58a05e6e7049b
Gerzain Mata <leftger@gmail.com>
2025-12-14 16:45:49 -0700

    Added missing constants

--- stm32-bindings-gen/inc/app_conf.h
+++ stm32-bindings-gen/inc/app_conf.h
@@ -37,6 +37,42 @@ extern "C" {
 #define RADIO_INTR_PRIO_LOW (0U)
 #endif
 
+#ifndef RADIO_SW_LOW_INTR_NUM
+#define RADIO_SW_LOW_INTR_NUM (0U)
+#endif
+
+#ifndef RADIO_SW_LOW_INTR_PRIO
+#define RADIO_SW_LOW_INTR_PRIO (0U)
+#endif
+
+#ifndef RCC_INTR_PRIO
+#define RCC_INTR_PRIO (0U)
+#endif
+
+#ifndef USE_RADIO_LOW_ISR
+#define USE_RADIO_LOW_ISR (1U)
+#endif
+
+#ifndef NEXT_EVENT_SCHEDULING_FROM_ISR
+#define NEXT_EVENT_SCHEDULING_FROM_ISR (1U)
+#endif
+
+#ifndef CFG_SCM_SUPPORTED
+#define CFG_SCM_SUPPORTED (1U)
+#endif
+
+#ifndef CFG_DEBUGGER_LEVEL
+#define CFG_DEBUGGER_LEVEL (0U)
+#endif
+
+#ifndef CFG_RT_DEBUG_GPIO_MODULE
+#define CFG_RT_DEBUG_GPIO_MODULE (0U)
+#endif
+
+#ifndef CFG_RT_DEBUG_DTB
+#define CFG_RT_DEBUG_DTB (0U)
+#endif
+
 #ifdef __cplusplus
 }
 #endif
